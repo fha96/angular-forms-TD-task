8e7f5e6693d8c989d3b1b2c291535edc2277b000
fha96 <fahadalrimawe96@hotmail.com>
2023-07-09 12:49:28 +0300

    complete assignment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'form-task';
+  defaultSelected: string = 'advanced';
+
+
+  onSubmit(formData: NgForm){
+    console.log(formData.value);
+    
+    formData.reset();
+    
+  }
 }
